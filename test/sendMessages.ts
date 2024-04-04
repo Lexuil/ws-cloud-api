661d7af22d79329475ff09f8e6cc3647d3cfeccc
Lexuil <ojumanab@unal.edu.co>
2024-04-04 15:53:00 -0500

    Change test to typescript

--- test/sendMessages.ts
+++ test/sendMessages.ts
@@ -1,18 +1,23 @@
-import { sendImage, sendText, sendVideo } from '../dist/index.mjs'
+import 'dotenv/config'
+import {
+  sendImage,
+  sendText,
+  sendVideo
+} from '../dist/index'
 
 const phoneNumberToTest = '573202601178'
 
 sendText(
   phoneNumberToTest,
   'Mensaje de prueba de librería'
-)
+).catch(console.error)
 
 sendImage(
   phoneNumberToTest,
   'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-)
+).catch(console.error)
 
 sendVideo(
   phoneNumberToTest,
   'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
-)
+).catch(console.error)
