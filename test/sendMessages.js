725637ec2b1b566cdf2b6efd1b9169194ea662f5
Lexuil <ojumanab@unal.edu.co>
2024-04-04 12:42:09 -0500

    Add simple send messages test with node

---
+++ test/sendMessages.js
@@ -0,0 +1,18 @@
+import { sendImage, sendText, sendVideo } from '../dist/my-lib.js'
+
+const phoneNumberToTest = '573202601178'
+
+sendText(
+  phoneNumberToTest,
+  'Mensaje de prueba de librería'
+)
+
+sendImage(
+  phoneNumberToTest,
+  'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+)
+
+sendVideo(
+  phoneNumberToTest,
+  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'
+)
