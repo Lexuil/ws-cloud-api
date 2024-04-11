145f8a62c33a745c1995e903019fe9f152179ffa
Lexuil <ojumanab@unal.edu.co>
2024-04-11 12:31:59 -0500

    Add some body examples and test

---
+++ test/webhook.ts
@@ -0,0 +1,13 @@
+import { handleWebhook } from '../dist'
+import textBody from './bodyExamples/messageText.json'
+import replyButtonBody from './bodyExamples/messageFromButton.json'
+import replyListBody from './bodyExamples/messageFromList.json'
+
+console.log('Text message')
+console.log(handleWebhook(textBody))
+
+console.log('\nReply button message')
+console.log(handleWebhook(replyButtonBody))
+
+console.log('\nReply list message')
+console.log(handleWebhook(replyListBody))
