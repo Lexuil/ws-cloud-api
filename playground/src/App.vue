f1f6ae3003f6f4728d2f6cb67efd946f2051d33f
Lexuil <ojumanab@unal.edu.co>
2024-05-02 23:36:49 -0500

    Add ESLint

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -4,11 +4,25 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <a
+      href="https://vitejs.dev"
+      target="_blank"
+    >
+      <img
+        src="/vite.svg"
+        class="logo"
+        alt="Vite logo"
+      >
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+    >
+      <img
+        src="./assets/vue.svg"
+        class="logo vue"
+        alt="Vue logo"
+      >
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -27,4 +41,5 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+</style>./components/Hello.vue
+./components/HelloWorld.vue
