641a770ff38416a1fccbd4dfc3c7be60e475a028
Lexuil <ojumanab@unal.edu.co>
2024-05-03 15:54:44 -0500

    Add text message component

---
+++ playground/src/components/Chat/TextMessage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  message: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <li class="p-2 bg-white shadow-sm rounded-md text-gray-700 w-fit mt-[2px]">
+    <span>
+      {{ message }}
+    </span>
+    <span class="invisible h-0 text-[0.68rem] p-1">{{ time }}</span>
+    <div class="relative text-[0.68rem] text-gray-500">
+      <span class="float-right mt-[-10px]">{{ time }}</span>
+    </div>
+  </li>
+</template>
--- playground/src/components/ChatView.vue
+++ playground/src/components/ChatView.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
+import { useMessagesStore } from '../stores/messagesStore'
+import TextMessage from './Chat/TextMessage.vue'
+
+const messages = useMessagesStore()
 </script>
 
 <template>
-  <div class="aspect-[9/16] w-96 bg-[url('/chat-background.png')] bg-repeat bg-[length:24rem]" />
+  <div
+    class="w-96 h-[42.7rem] bg-[url('/chat-background.png')] bg-repeat bg-[length:24rem] rounded-lg px-10 py-2 overflow-auto"
+    style="scrollbar-width: thin;"
+  >
+    <ol>
+      <template v-for="(message, index) in messages.messages">
+        <TextMessage
+          v-if="message.type === 'text'"
+          :key="index"
+          :message="message.text"
+          time="2:45 p.m."
+        />
+      </template>
+    </ol>
+  </div>
 </template>
