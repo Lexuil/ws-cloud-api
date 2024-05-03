c047b86822ee05a031241763a6ab9836ede7e50c
Lexuil <ojumanab@unal.edu.co>
2024-05-03 12:22:03 -0500

    Add phone number to send in config form

--- playground/src/components/ConfigForm.vue
+++ playground/src/components/ConfigForm.vue
@@ -3,14 +3,14 @@ import { ref } from 'vue'
 
 const phoneNumberId = ref('')
 const token = ref('')
+const phoneNumberTo = ref('')
 </script>
 
 <template>
-  <div class="flex flex-col items-center space-y-4 w-96 h-fit border border-gray-300 p-5 rounded-md">
+  <div class="flex flex-col items-center gap-3 w-96 h-fit border border-gray-300 p-5 rounded-md">
     <label
-      class="text-lg"
       for="phoneNumberId"
-    >Phone Number ID</label>
+    >Phone number ID</label>
     <input
       id="phoneNumberId"
       v-model="phoneNumberId"
@@ -20,13 +20,23 @@ const token = ref('')
     >
 
     <label
-      class="text-lg"
       for="token"
     >Token</label>
     <input
       id="token"
       v-model="token"
       name="token"
+      type="text"
+      class="border border-gray-300 rounded-md p-2 w-full"
+    >
+
+    <label
+      for="phoneNumberTo"
+    >Phone number to test</label>
+    <input
+      id="phoneNumberTo"
+      v-model="phoneNumberTo"
+      name="phoneNumberTo"
       type="text"
       class="border border-gray-300 rounded-md p-2 w-full"
     >
