70492417303f190fc119d4f449c43a469cfc555e
Lexuil <ojumanab@unal.edu.co>
2024-05-04 12:50:38 -0500

    Add send messages button to ChatView component

--- playground/src/components/ChatView.vue
+++ playground/src/components/ChatView.vue
@@ -1,38 +1,56 @@
 <script setup lang="ts">
-import { useMessagesStore } from '../stores/messagesStore'
 import TextMessage from './Chat/TextMessage.vue'
 import ImageMessage from './Chat/ImageMessage.vue'
 import VideoMessage from './Chat/VideoMessage.vue'
+import { useMessagesStore } from '../stores/messagesStore'
+import { useConfigStore } from '../stores/configStore'
+import useSendMessages from '../hooks/useSendMessages'
 
 const messages = useMessagesStore()
+const config = useConfigStore()
+const { sendMessages } = useSendMessages()
 </script>
 
 <template>
   <div
-    class="w-96 h-[42.7rem] bg-[url('/chat-background.png')] bg-repeat bg-[length:24rem] rounded-lg px-10 py-2 overflow-auto"
-    style="scrollbar-width: thin;"
+    class="flex flex-col items-center gap-3"
   >
-    <ol>
-      <template v-for="(message, index) in messages.messages">
-        <TextMessage
-          v-if="message.type === 'text'"
-          :key="index + '-text'"
-          :message="message.text"
-          time="2:45 p.m."
-        />
-        <ImageMessage
-          v-else-if="message.type === 'image'"
-          :key="index + '-image'"
-          :link="message.link"
-          time="2:45 p.m."
-        />
-        <VideoMessage
-          v-else-if="message.type === 'video'"
-          :key="index + '-video'"
-          :link="message.link"
-          time="2:45 p.m."
-        />
-      </template>
-    </ol>
+    <div
+      class="w-96 h-[42.7rem] bg-[url('/chat-background.png')] bg-repeat bg-[length:24rem] rounded-lg px-10 py-2 overflow-auto relative"
+      style="scrollbar-width: thin;"
+    >
+      <ol>
+        <template v-for="(message, index) in messages.messages">
+          <TextMessage
+            v-if="message.type === 'text'"
+            :key="index + '-text'"
+            :message="message.text"
+            time="2:45 p.m."
+          />
+          <ImageMessage
+            v-else-if="message.type === 'image'"
+            :key="index + '-image'"
+            :link="message.link"
+            time="2:45 p.m."
+          />
+          <VideoMessage
+            v-else-if="message.type === 'video'"
+            :key="index + '-video'"
+            :link="message.link"
+            time="2:45 p.m."
+          />
+        </template>
+      </ol>
+    </div>
+    <button
+      class="bg-blue-500 text-white px-3 py-1 rounded-md disabled:bg-gray-300"
+      :disabled="config.phoneNumberId === '' ||
+        config.token === '' ||
+        config.phoneNumberTo === '' ||
+        messages.messages.length === 0"
+      @click="sendMessages(messages.messages)"
+    >
+      Send messages
+    </button>
   </div>
 </template>
