cdd2bfc510f230925034a39b89544146f9e7b09f
Lexuil <ojumanab@unal.edu.co>
2024-05-05 11:57:53 -0500

    Extract chat body on ChatBody component

---
+++ playground/src/components/Chat/ChatBody.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import TextMessage from '../Chat/TextMessage.vue'
+import ImageMessage from '../Chat/ImageMessage.vue'
+import VideoMessage from '../Chat/VideoMessage.vue'
+import { useMessagesStore } from '../../stores/messagesStore'
+
+const messages = useMessagesStore()
+</script>
+
+<template>
+  <div
+    class="w-full sm:w-96 h-[42.7rem] bg-[url('/chat-background.png')] bg-repeat bg-[length:24rem] rounded-b-lg px-10 py-2 overflow-auto relative"
+    style="scrollbar-width: thin;"
+  >
+    <ol>
+      <template v-for="(message, index) in messages.messages">
+        <TextMessage
+          v-if="message.type === 'text'"
+          :key="index + '-text'"
+          :message="message.text"
+          time="2:45 p.m."
+        />
+        <ImageMessage
+          v-else-if="message.type === 'image'"
+          :key="index + '-image'"
+          :link="message.link"
+          time="2:45 p.m."
+        />
+        <VideoMessage
+          v-else-if="message.type === 'video'"
+          :key="index + '-video'"
+          :link="message.link"
+          time="2:45 p.m."
+        />
+      </template>
+    </ol>
+  </div>
+</template>
--- playground/src/components/ChatView.vue
+++ playground/src/components/ChatView.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import ChatHeader from './Chat/ChatHeader.vue'
-import TextMessage from './Chat/TextMessage.vue'
-import ImageMessage from './Chat/ImageMessage.vue'
-import VideoMessage from './Chat/VideoMessage.vue'
+import ChatBody from './Chat/ChatBody.vue'
 import { useMessagesStore } from '../stores/messagesStore'
 import { useConfigStore } from '../stores/configStore'
 import useSendMessages from '../hooks/useSendMessages'
@@ -17,34 +15,7 @@ const { sendMessages } = useSendMessages()
     class="flex flex-col items-center max-sm:w-full max-sm:p-4"
   >
     <ChatHeader />
-
-    <div
-      class="w-full sm:w-96 h-[42.7rem] bg-[url('/chat-background.png')] bg-repeat bg-[length:24rem] rounded-b-lg px-10 py-2 overflow-auto relative"
-      style="scrollbar-width: thin;"
-    >
-      <ol>
-        <template v-for="(message, index) in messages.messages">
-          <TextMessage
-            v-if="message.type === 'text'"
-            :key="index + '-text'"
-            :message="message.text"
-            time="2:45 p.m."
-          />
-          <ImageMessage
-            v-else-if="message.type === 'image'"
-            :key="index + '-image'"
-            :link="message.link"
-            time="2:45 p.m."
-          />
-          <VideoMessage
-            v-else-if="message.type === 'video'"
-            :key="index + '-video'"
-            :link="message.link"
-            time="2:45 p.m."
-          />
-        </template>
-      </ol>
-    </div>
+    <ChatBody />
 
     <div class="flex w-full justify-center gap-5 mt-5">
       <button
