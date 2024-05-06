45070a9ffaeed7f794cba4b8f94fa0301aaa8a17
Lexuil <ojumanab@unal.edu.co>
2024-05-06 15:42:37 -0500

    Fix webhook verification

--- src/webhook.ts
+++ src/webhook.ts
@@ -6,7 +6,7 @@ export function verifyWebhook (input: WebhookSubscribeQuery): {
   statusCode: 200 | 401
   body?: string
 } {
-  if (input['hub.mode'] !== 'subscribe' &&
+  if (input['hub.mode'] !== 'subscribe' ||
   input['hub.verify_token'] !== process.env.WS_VERIFY_TOKEN) {
     return {
       statusCode: 401
