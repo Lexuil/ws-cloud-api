ddbf8f897abe971b6d7eb63287e72ddd5109f2a1
Lexuil <ojumanab@unal.edu.co>
2024-05-06 19:39:43 -0500

    Add dropdown component

---
+++ playground/src/components/Ui/Dropdown/UiDropdown.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import useDropdown from '@/composables/useDropdown'
+const { closeMenu, menu, menuIsOpen } = useDropdown()
+
+defineProps({
+  triggerClass: {
+    type: String,
+    required: true
+  },
+  dropdownClass: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <details
+    ref="menu"
+    class="relative"
+    @click="menuIsOpen = !menuIsOpen"
+  >
+    <!-- Trigger -->
+    <summary
+      :class="triggerClass"
+    >
+      <slot name="trigger" />
+    </summary>
+
+    <!-- Popup -->
+    <ul
+      v-show="menuIsOpen"
+      :class="dropdownClass"
+      @click.stop="closeMenu"
+    >
+      <slot />
+    </ul>
+  </details>
+</template>
