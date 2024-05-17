7e7bcdd27976c3d384130faa94c41df792d68130
Lexuil <ojumanab@unal.edu.co>
2024-05-17 17:37:05 -0500

    feat: Add audio media handling to webhook test

--- src/test/webhook.ts
+++ src/test/webhook.ts
@@ -4,6 +4,8 @@ import textBody from './bodyExamples/messageText.json'
 import replyButtonBody from './bodyExamples/messageFromButton.json'
 import replyListBody from './bodyExamples/messageFromList.json'
 import voiceAudioBody from './bodyExamples/messageVoiceAudio.json'
+import { getMedia, getMediaUrl } from '../../dist/media'
+import fs from 'fs'
 
 console.log('Text message')
 console.log(handleWebhook(textBody))
@@ -14,5 +16,24 @@ console.log(handleWebhook(replyButtonBody))
 console.log('\nReply list message')
 console.log(handleWebhook(replyListBody))
 
+// Test audio message
 console.log('\nVoice audio message')
-console.log(handleWebhook(voiceAudioBody))
+const audioResponse = handleWebhook(voiceAudioBody)
+if (audioResponse?.type === 'voiceAudio') {
+  getMediaUrl({ mediaId: audioResponse.audio.id })
+    .then(mediaUrl => {
+      console.log(mediaUrl)
+      getMedia({ mediaUrl })
+        .then(media => {
+          // Save blob media to file
+          media.arrayBuffer()
+            .then(arrayBuffer => {
+              const array = new Uint8Array(arrayBuffer)
+              fs.writeFileSync('audio.ogg', Buffer.from(array))
+            })
+            .catch(console.error)
+        })
+        .catch(console.error)
+    })
+    .catch(console.error)
+}
