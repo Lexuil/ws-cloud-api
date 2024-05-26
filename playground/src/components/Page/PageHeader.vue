13cc25080fa9a5ad1baad273b55c62cd6b487d13
Lexuil <ojumanab@unal.edu.co>
2024-05-26 17:13:51 -0500

    feat: Add dark mode toggle to PageHeader component

--- playground/src/components/Page/PageHeader.vue
+++ playground/src/components/Page/PageHeader.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
 <template>
   <header class="flex items-center justify-between w-full h-20 sm:h-12 bg-emerald-700 text-white px-5">
     <h1 class="text-2xl">
@@ -35,6 +42,34 @@
           class="underline"
         >Lexuil</a>
       </span>
+
+      <!-- Dark mode toggle -->
+      <button
+        @click="toggleDark()"
+      >
+        <svg
+          v-if="isDark"
+          class="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+        ><path
+          fill="currentColor"
+          d="M12.058 20q-3.334 0-5.667-2.333T4.058 12q0-3.038 1.98-5.27Q8.02 4.5 10.942 4.097q.081 0 .159.006t.153.017q-.506.706-.801 1.57t-.295 1.81q0 2.667 1.866 4.533t4.534 1.867q.952 0 1.813-.295q.862-.295 1.548-.801q.012.075.018.153t.005.158q-.384 2.923-2.615 4.904T12.057 20"
+        /></svg>
+        <svg
+          v-else
+          class="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 24 24"
+        ><path
+          fill="currentColor"
+          d="M12 15q1.25 0 2.125-.875T15 12t-.875-2.125T12 9t-2.125.875T9 12t.875 2.125T12 15m0 1q-1.671 0-2.836-1.164T8 12t1.164-2.836T12 8t2.836 1.164T16 12t-1.164 2.836T12 16m-7-3.5H1.5v-1H5zm17.5 0H19v-1h3.5zM11.5 5V1.5h1V5zm0 17.5V19h1v3.5zM6.746 7.404l-2.16-2.098l.695-.744l2.111 2.134zM18.72 19.438l-2.117-2.14l.652-.702l2.16 2.098zM16.596 6.746l2.098-2.16l.744.695l-2.134 2.111zM4.562 18.72l2.14-2.117l.663.652l-2.078 2.179zM12 12"
+        /></svg>
+      </button>
     </div>
   </header>
 </template>
