37142f1953b2df4be736afa487e4d557cb37d0f4
Lexuil <ojumanab@unal.edu.co>
2024-05-26 15:41:38 -0500

    refactor: Update PageHeader component styling to accommodate different screen sizes

--- playground/src/components/Page/PageHeader.vue
+++ playground/src/components/Page/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex items-center justify-between w-full h-12 bg-emerald-700 text-white px-5">
+  <header class="flex items-center justify-between w-full h-20 sm:h-12 bg-emerald-700 text-white px-5">
     <h1 class="text-2xl">
       ws-cloud-api Playground
     </h1>
