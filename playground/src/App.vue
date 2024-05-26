27e0a687f138c8a4c4773ceca2515e025d7ddaac
Lexuil <ojumanab@unal.edu.co>
2024-05-26 13:55:41 -0500

    refactor: Update component imports in App.vue and add ControlsView component

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,25 +1,22 @@
 <script setup lang="ts">
 import { Toaster } from 'vue-sonner'
-import ChatView from '@/components/ChatView.vue'
-import ConfigForm from '@/components/ConfigForm.vue'
-import SendMessagesButtons from '@/components/SendMessagesButtons.vue'
-import AddButtonsMessageModal from './components/AddMessage/AddButtonsMessageModal.vue'
-import AddListMessageModal from './components/AddMessage/AddListMessageModal.vue'
-import PageHeader from './components/Page/PageHeader.vue'
+import ChatView from '@/components/Chat/ChatView.vue'
+import ControlsView from '@/components/Controls/ControlsView.vue'
+import AddButtonsMessageModal from '@/components/AddMessage/AddButtonsMessageModal.vue'
+import AddListMessageModal from '@/components/AddMessage/AddListMessageModal.vue'
+import PageHeader from '@/components/Page/PageHeader.vue'
 </script>
 
 <template>
+  <!-- Components -->
   <Toaster />
   <AddButtonsMessageModal />
   <AddListMessageModal />
+
+  <!-- Page -->
   <PageHeader />
-  <main class="flex flex-col items-center justify-center w-full">
-    <div class="flex flex-wrap justify-center gap-5 w-full my-7">
-      <div class="flex flex-col items-center gap-5 max-sm:w-full max-sm:p-4">
-        <ConfigForm />
-        <SendMessagesButtons />
-      </div>
-      <ChatView />
-    </div>
+  <main class="flex flex-wrap justify-center gap-5 w-full my-7">
+    <ControlsView />
+    <ChatView />
   </main>
 </template>
---
+++ playground/src/components/Controls/ControlsView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import ConfigForm from './ConfigForm.vue'
+import SendMessagesButtons from './SendMessagesButtons.vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-5 max-sm:w-full max-sm:p-4">
+    <ConfigForm />
+    <SendMessagesButtons />
+  </div>
+</template>
