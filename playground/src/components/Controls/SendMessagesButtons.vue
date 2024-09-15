8cdb914b44537dd19ad8b965c5061ca1839ea85b
Lexuil <ojumanab@unal.edu.co>
2024-09-15 12:16:27 -0500

    Refactor SendMessagesButtons

--- playground/src/components/Controls/SendMessagesButtons.vue
+++ playground/src/components/Controls/SendMessagesButtons.vue
@@ -1,4 +1,13 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { Send } from 'lucide-vue-next'
 import { useMessagesStore } from '@/stores/messagesStore'
 import useSendMessages from '@/composables/useSendMessages'
 
@@ -7,24 +16,29 @@ const { sendMessages, sendingMessages, availableToSend } = useSendMessages()
 </script>
 
 <template>
-  <div class="box">
-    <h2 class="text-2xl font-bold mb-5">
-      Messaging
-    </h2>
-
-    <button
-      class="bg-primary"
-      :disabled="availableToSend || messages.messages.length === 0"
-      @click="sendMessages(messages.messages)"
-    >
-      {{ sendingMessages ? 'Sending...' : 'Send messages' }}
-    </button>
-    <button
-      class="bg-error"
-      :disabled="messages.messages.length === 0"
-      @click="messages.clearMessages()"
-    >
-      Clear messages
-    </button>
-  </div>
+  <Card class="w-96 max-w-full">
+    <CardHeader>
+      <CardTitle class="flex items-center">
+        <Send class="inline w-5 h-5 mr-2" /> Messaging
+      </CardTitle>
+      <CardDescription>Send messages to test</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <div class="flex flex-wrap justify-around gap-3">
+        <Button
+          :disabled="availableToSend || messages.messages.length === 0"
+          @click="sendMessages(messages.messages)"
+        >
+          {{ sendingMessages ? 'Sending...' : 'Send messages' }}
+        </Button>
+        <Button
+          variant="destructive"
+          :disabled="messages.messages.length === 0"
+          @click="messages.clearMessages()"
+        >
+          Clear messages
+        </Button>
+      </div>
+    </CardContent>
+  </Card>
 </template>
