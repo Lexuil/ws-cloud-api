b63306b5f45e734ad226b4ee3ed061dacf1c98f6
Lexuil <ojumanab@unal.edu.co>
2024-10-02 11:42:05 -0500

    Fix enums export

--- ws-cloud-api/src/index.ts
+++ ws-cloud-api/src/index.ts
@@ -2,7 +2,7 @@
 export type * from './types/messages'
 export type * from './types/config'
 export type * from './types/webhook'
-export type * from './types/enums'
 export type * from './types/webhook/media'
 export type * from './types/webhook/messages'
 export type * from './types/webhook/statuses'
+export * from './types/enums'
